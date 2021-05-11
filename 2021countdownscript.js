646f272c76895513016273dcd091b9a90824df5c
LuisAngelMaciel <69272932+hiLuisXiib@users.noreply.github.com>
2021-05-11 13:12:50 -0500

    Update 2021countdownscript.js

--- 2021countdownscript.js
+++ 2021countdownscript.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Jan 1, 2021 00:00:00").getTime();
+var countDownDate = new Date("Jan 1, 2022 00:00:00").getTime();
 
 var x = setInterval(function() {
   var now = new Date().getTime();
